eddf3f735ef71c02b31935ba865cc9e0d445ba9c
Ekateryna <kate7smith12@gmail.com>
2023-12-12 13:45:01 +0100

    cd output for gnark

--- src/bin/prove_recursive.rs
+++ src/bin/prove_recursive.rs
@@ -1,22 +1,22 @@
-use std::fs::File;
-use std::{env, fs};
-use std::io::{ Write, BufWriter};
-use std::marker::PhantomData;
-use plonky2::field::goldilocks_field::GoldilocksField;
-use plonky2::plonk::config::PoseidonGoldilocksConfig;
 use anyhow::Result;
-use env_logger::{DEFAULT_FILTER_ENV, Env, try_init_from_env};
+use env_logger::{try_init_from_env, Env, DEFAULT_FILTER_ENV};
 use log::{error, info};
-use plonky2::util::serialization::{DefaultGateSerializer, DefaultGeneratorSerializer};
-use plonky2::util::timing::TimingTree;
-use maru_volume_stark::circom_verifier::{generate_verifier_config, generate_proof_base64};
-use serde::{Deserialize, Serialize};
 use maru_volume_stark::all_stark::AllStark;
 use maru_volume_stark::block_header::read_headers_from_file;
+use maru_volume_stark::circom_verifier::{generate_proof_base64, generate_verifier_config};
 use maru_volume_stark::config::StarkConfig;
 use maru_volume_stark::fixed_recursive_verifier::AllRecursiveCircuits;
 use maru_volume_stark::generation::PatriciaInputs;
 use maru_volume_stark::patricia_merkle_trie::{convert_to_tree, read_paths_from_file};
+use plonky2::field::goldilocks_field::GoldilocksField;
+use plonky2::plonk::config::PoseidonGoldilocksConfig;
+use plonky2::util::serialization::{DefaultGateSerializer, DefaultGeneratorSerializer};
+use plonky2::util::timing::TimingTree;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::marker::PhantomData;
+use std::{env, fs};
 
 type F = GoldilocksField;
 
@@ -29,8 +29,6 @@ type C = PoseidonGoldilocksConfig;
 pub struct ConfigJson {
     pub degree_bits: String,
 }
-
-
 
 pub fn generate_proof(
     file_name: &str,
@@ -46,7 +44,8 @@ pub fn generate_proof(
     };
 
     let all_paths = read_paths_from_file(path_file).expect("Reading paths error");
-    let block_headers = read_headers_from_file(blockheaders_file).expect("Reading block headers error");
+    let block_headers =
+        read_headers_from_file(blockheaders_file).expect("Reading block headers error");
     let tries = convert_to_tree(&all_paths)?;
     let patricia_inputs = PatriciaInputs {
         pmt: tries,
@@ -56,19 +55,19 @@ pub fn generate_proof(
 
     let config = StarkConfig::standard_fast_config();
     let all_stark = AllStark::<F, D>::default();
-    let recursive_circuit:  AllRecursiveCircuits<GoldilocksField, C, 2> = AllRecursiveCircuits::from_bytes(
-        &binary_data,
-        &gate_serializer,
-        &generator_serializer,
-    ).unwrap();
+    let recursive_circuit: AllRecursiveCircuits<GoldilocksField, C, 2> =
+        AllRecursiveCircuits::from_bytes(&binary_data, &gate_serializer, &generator_serializer)
+            .unwrap();
     let mut timing = TimingTree::new("Generate recursive proof", log::Level::Error);
-    let (root_proof, pis) = recursive_circuit.prove_root(
-        &all_stark,
-        &config,
-        Default::default(),
-        patricia_inputs.clone(),
-        &mut timing,
-    ).expect("Proving error");
+    let (root_proof, pis) = recursive_circuit
+        .prove_root(
+            &all_stark,
+            &config,
+            Default::default(),
+            patricia_inputs.clone(),
+            &mut timing,
+        )
+        .expect("Proving error");
     timing.print();
 
     let final_proof_path = format!("{}", proof_file);
@@ -76,6 +75,12 @@ pub fn generate_proof(
     let mut proof_bytes = root_proof.to_bytes();
     proof_bytes.push(is_aggregated);
     fs::write(final_proof_path, &proof_bytes).expect("Proof writing error");
+
+    let gnark_cd = String::from("common_circuit_data.json");
+    let gnark_cd_file = File::create(gnark_cd)?;
+    let mut writer = BufWriter::new(gnark_cd_file);
+    serde_json::to_writer(&mut writer, &root_proof)?;
+
     // let conf = generate_verifier_config(&root_proof).expect("Generate verifier config error");
     // let proof_base64_json = generate_proof_base64(&root_proof, &conf).expect("Generate proof Base64 error");
     // let pretty_proof_path = format!("{}.json", final_proof_path);
@@ -97,14 +102,18 @@ fn main() -> Result<()> {
     init_logger();
     let args: Vec<String> = env::args().collect();
     if args.len() != 5 {
-        error!("Usage: {} <circuit_file> <path_file> <blockheaders_file> <proof_file>", args[0]);
+        error!(
+            "Usage: {} <circuit_file> <path_file> <blockheaders_file> <proof_file>",
+            args[0]
+        );
         std::process::exit(1);
     }
     let circuit_file = &args[1];
     let path_file = &args[2];
     let blockheaders_file = &args[3];
     let proof_file = &args[4];
-    generate_proof(circuit_file, path_file, blockheaders_file, proof_file).expect("Proof generation error");
+    generate_proof(circuit_file, path_file, blockheaders_file, proof_file)
+        .expect("Proof generation error");
     info!("OK");
     Ok(())
 }
